6d525dc68afd86cfd3889ffd5cac5b479cad4437
J-rgb-ai <opiyojoyshaney@gmail.com>
2024-10-01 18:38:16 +0000

    node js

---
+++ 0x05-Node_JS_basic/.eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  rules: {
+    // Add any specific rules here
+    'indent': ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always'],
+  },
+};
